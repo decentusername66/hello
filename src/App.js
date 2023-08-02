cd764fdd2ba1f37bf8ceb5552b3c7083f5d24bae
gosia <wheresmyredpillmorpheus@gmail.com>
2023-08-02 12:29:35 +0200

    nice

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import './App.css';
 import Employee from './components/Employee';
+import {useState} from 'react';
 
 function App() {
+  const [role, setRole] = useState();
    const showEmployees=true;
   return (
     <div className="App">
       {showEmployees ? (
       <>
-        <Employee />
-        <Employee />
-        <Employee />
-        <Employee />
+      <input 
+        type='text' 
+        onChange={(e)=> {
+          console.log(e.target.value);
+          setRole(e.target.value);
+      }}
+      />
+        <Employee name = "Marta" role = "Intern" />
+        <Employee name = "Anna"  role={role}/>
+        <Employee name = "Eryk"/>
         </>
   ):(
             <p>

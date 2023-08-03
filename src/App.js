62c566b50279927e59fd38373065b2532e12859e
gosia <wheresmyredpillmorpheus@gmail.com>
2023-08-03 21:34:29 +0200

    functioning update and pretty card:;

--- src/App.js
+++ src/App.js
@@ -7,25 +7,42 @@ function App() {
   const [role, setRole] = useState();
   const[employees, setEmployees]=useState(
     [
-      {name: "Anna", 
+      {id:1,
+        name: "Anna", 
       role: "Developer", 
       img: "https://images.pexels.com/photos/1181686/pexels-photo-1181686.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1/"},
-      {name: "Steven", 
+      {id:2,
+      name: "Steven", 
       role: "SCRUM Master", 
       img: "https://images.pexels.com/photos/2380794/pexels-photo-2380794.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1/"},
-      {name: "Marta", 
+      {id:3,
+      name: "Marta", 
       role: "Data Analyst", 
       img: "https://images.pexels.com/photos/1727273/pexels-photo-1727273.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1/"},
-      {name: "Malgorzata", 
+      {id:4,
+      name: "Malgorzata", 
       role: "Developer", 
       img: "https://images.pexels.com/photos/3886347/pexels-photo-3886347.jpeg?auto=compress&cs=tinysrgb&w=600/"},
-      {name: "Mariusz", 
+      {id:5,
+      name: "Mariusz", 
       role: "Senior Developer", 
       img: "https://images.pexels.com/photos/3008355/pexels-photo-3008355.jpeg?auto=compress&cs=tinysrgb&w=600/"},
-      {name: "Dariusz", 
+      {id:6,
+      name: "Dariusz", 
       role: "Senior DB Developer", 
       img: "https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg?auto=compress&cs=tinysrgb&w=600/"},
    ]);
+
+function updateEmployee(id, newName, newRole) {
+  const updatedEmployees= employees.map((employee) => {
+  if (id == employee.id) {
+    return {...employee, name: newName, role: newRole};
+    }
+
+    return employee;
+  });
+  setEmployees(updatedEmployees);
+}
 
    const showEmployees=true;
   return (
@@ -41,12 +58,14 @@ function App() {
     />
       <div className="flex flex-wrap justify-center">
         {employees.map((employee)=>{
-          console.log(employee);
-          return(<Employee 
-              key={uuidv4()}
-              name={employee.name} 
-              role={employee.role} 
-              img={employee.img}
+          //console.log(employee);
+              return(<Employee 
+                  key={employee.id}
+                  id={employee.id}
+                  name={employee.name} 
+                  role={employee.role} 
+                  img={employee.img}
+                  updateEmployee = {updateEmployee}
     />
           );
         })}
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -2,7 +2,7 @@ import EditEmployee from "./EditEmployee";
 
 function Employee(props){
     return (
-        <div className="m-2 py-8 px-8 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+        <div className="min-w-[350px] max-w-[350] m-2 py-8 px-8 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
             <img className="object-cover rounded-full h-[100px] w-[100px] block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" src={props.img} alt="Woman's Face" />
             <div className="text-center space-y-2 sm:text-left">
             <div className="space-y-0.5">
@@ -11,8 +11,13 @@ function Employee(props){
 
             </div>
 
-            <EditEmployee />
-            
+            <EditEmployee 
+            id={props.id}
+            name={props.name} 
+            role={props.role} 
+            updateEmployee={props.updateEmployee}
+            />
+
             </div>
         </div>
         );
